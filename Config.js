2dead72099129baba7a1100743398d3f7bac16e2
evansjay2193 <149078330+evansjay2193@users.noreply.github.com>
2024-03-14 22:15:50 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['+254759817301']
+global.ownername = "Jay 📌"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "kenya"
 
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
